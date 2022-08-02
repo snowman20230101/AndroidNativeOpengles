559461806bd20b234ca85e4f7a441d502ef6fab7
wuwenbin <wuwenbin@okay.cn>
2022-08-02 15:05:24 +0800

    feat: init AndroidOpenGLES project.

---
+++ app/src/main/java/com/windy/opengles/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.windy.opengles
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import com.windy.opengles.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Example of a call to a native method
+        binding.sampleText.text = stringFromJNI()
+    }
+
+    /**
+     * A native method that is implemented by the 'opengles' native library,
+     * which is packaged with this application.
+     */
+    external fun stringFromJNI(): String
+
+    companion object {
+        // Used to load the 'opengles' library on application startup.
+        init {
+            System.loadLibrary("opengles")
+        }
+    }
+}
